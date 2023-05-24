1051f38d2f48ee0813c64e3ee61aaad839dc4918
Seuhyang <yunsu.choi@monoverse.io>
2023-05-24 18:07:48 +0900

    creating sample data

---
+++ db/create.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE common;
+
+CREATE TABLE common.teachers (
+    id int PRIMARY KEY AUTO_INCREMENT,
+    teacher_name varchar(50) NOT NULL,
+    teacher_position varchar(50) DEFAULT 'General Teacher',
+    create_date datetime DEFAULT current_timestamp,
+    update_date datetime ON UPDATE current_timestamp
+);
+
+CREATE TABLE common.student (
+    id int PRIMARY KEY AUTO_INCREMENT,
+    name varchar(20) NOT NULL,
+    grade int(1) COMMENT 'student grade',
+    create_date datetime DEFAULT current_timestamp,
+    update_date datetime ON UPDATE current_timestamp
+);
+
+CREATE TABLE common.student_data (
+    id int PRIMARY KEY AUTO_INCREMENT,
+    std_id int not NULL,
+    address varchar(100),
+    phone varchar(20),
+    parent_phone varchar(20) ,
+    birth_date varchar(20),
+    join_club_id int COMMENT 'join club id',
+    create_date datetime DEFAULT current_timestamp,
+    update_date datetime ON UPDATE current_timestamp
+);
+
+CREATE TABLE common.club (
+    id int PRIMARY KEY AUTO_INCREMENT,
+    club_name varchar(20) NOT NULL,
+    room_number int COMMENT COMMENT 'club room number',
+    rep_std_id int COMMENT 'a representative student of the club',
+    create_date datetime DEFAULT current_timestamp,
+    update_date datetime ON UPDATE current_timestamp
+);
+
+CREATE TABLE common.class_name (
+    id int PRIMARY KEY AUTO_INCREMENT,
+    class_number int UNIQUE KEY COMMENT 'student class number',
+    teacher_id int NOT NULL
+);
---
+++ db/insert.sql
@@ -0,0 +1,91 @@
+INSERT INTO common.teachers (teacher_name, teacher_position)
+VALUES
+('Companion Polapo Chipmunk Singer', 'The Chairman of the Board'),
+('Karina Rocket Puncher', 'The Principal'),
+('Winter Armamenter', 'Vice Principal')
+;
+
+INSERT INTO common.student (name, grade)
+VALUES
+( 'John David', 1),
+( 'Cloud Katharine', 1),
+( 'John von Neumann', 1),
+( 'Boris Jineman', 1),
+( 'Charlotte Bietris de Orlanne', 1),
+( 'Rusian Kalts', 1),
+( 'Hazel', 1),
+( 'Grace', 1),
+( 'Amelia', 1),
+( 'Olivia', 1),
+( 'Oskar', 1),
+( 'Tyler', 1),
+( 'Jade', 1),
+( 'Elliot', 1),
+( 'Lucas', 1),
+( 'Liam', 1),
+( 'Harry', 1),
+( 'Ava', 1),
+( 'Riley', 1),
+( 'Layla', 1),
+( 'Abigail', 1),
+( 'Isabella', 1),
+( 'Ella', 1),
+
+
+( 'Emma', 2),
+( 'Herry', 2),
+( 'Wyatt', 2),
+( 'William', 2),
+( 'Jack', 2),
+( 'Noah', 2),
+( 'Levi', 2),
+( 'Mason', 2),
+( 'Asher', 2),
+( 'Mia', 2),
+( 'Sophia', 2),
+( 'Luna', 2),
+( 'Harper', 2),
+( 'Gianna', 2),
+( 'Evelyn', 2),
+( 'Eille', 2),
+( 'Milla', 2),
+( 'Sofia', 2),
+( 'Isla', 2),
+( 'Violet', 2),
+( 'Emilly', 2),
+( 'Stella', 2),
+( 'Paisley', 2),
+
+
+( 'Willow', 3),
+( 'Ivy', 3),
+( 'Zoe', 3),
+( 'Lucy', 3),
+( 'Delilah', 3),
+( 'Skylar', 3),
+( 'Ayla', 3),
+( 'Leah', 3),
+( 'Iris', 3),
+( 'Melody', 3),
+( 'Tichiel Juspian', 3),
+( 'Cloe da Pontina', 3),
+( 'Nayatrei', 3),
+( 'Anais del Caril', 3),
+( 'Mila Nebraska', 3),
+( 'Lanziee Rosen Kranz', 3),
+( 'Benya', 3),
+( 'Isolesty', 3),
+( 'Emily Browning', 3),
+( 'Scarlett Johanson', 3),
+( 'Cloudy', 3),
+( 'Jenny', 3),
+( 'Katarina du Winter', 3),
+( 'Chloë Grace Moretz', 3),
+;
+
+INSERT INTO common.club (club_name, room_number, rep_std_id)
+VALUES
+('Student Body', 501)
+;
+
+INSERT INTO common.class_name;
